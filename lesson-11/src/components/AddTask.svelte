daf75d52facc1745266a65d035f065d2f58cad41
Michael B. <proxza@gmail.com>
2024-03-19 13:35:44 +1100

    Major updates for todo-app on svelte

--- lesson-11/src/components/AddTask.svelte
+++ lesson-11/src/components/AddTask.svelte
@@ -2,7 +2,7 @@
   import { tasks } from "./store";
 
   let title = "";
-  let priority = 1;
+  let priority;
   let deadline = "";
 
   function addNewTask() {
@@ -11,16 +11,16 @@
     tasks.addTask({ id: Date.now(), title, priority: Number(priority), deadline: newDateString, status: false });
     // Form cleaning
     title = "";
-    priority = 1;
+    priority = "";
     deadline = "";
   }
 </script>
 
 <section class="add-task">
   <form on:submit|preventDefault={addNewTask}>
-    <input type="text" id="new-task" placeholder="What needs to be done?" bind:value={title} />
+    <input type="text" id="new-task" placeholder="What needs to be done?" bind:value={title} autofocus />
     <input type="number" id="priority" placeholder="Priority" min="1" max="5" bind:value={priority} />
-    <input type="date" id="deadline" bind:value={deadline} />
+    <input type="date" id="deadline" placeholder="Deadline" bind:value={deadline} />
     <button type="submit">ADD</button>
   </form>
 </section>
--- lesson-11/src/components/ShowList.svelte
+++ lesson-11/src/components/ShowList.svelte
@@ -8,29 +8,27 @@
   let editedPriority = "";
   let editedDeadline = "";
 
-  // Sort state
+  let searchQuery = writable(""); // Variable for the search query
+
   const sortState = writable({ key: "title", direction: "ascending" });
 
-  // Derived store to sort tasks based on sort state
-  const sortedTasks = derived([tasks, sortState], ([$tasks, $sortState]) => {
-    return $tasks.slice().sort((a, b) => {
-      if ($sortState.key === "deadline") {
-        // Handle dates separately
-        const dateA = new Date(a[$sortState.key]),
-          dateB = new Date(b[$sortState.key]);
-        return $sortState.direction === "ascending" ? dateA - dateB : dateB - dateA;
-      } else {
-        // Handle strings and numbers generically
-        const valA = a[$sortState.key],
-          valB = b[$sortState.key];
-        if (valA < valB) return $sortState.direction === "ascending" ? -1 : 1;
-        if (valA > valB) return $sortState.direction === "ascending" ? 1 : -1;
-        return 0;
-      }
-    });
+  const filterStatus = writable("all"); // 'all', 'completed', 'notCompleted'
+
+  // Combined derived storage for filtering and sorting
+  const filteredAndSortedTasks = derived([tasks, searchQuery, sortState, filterStatus], ([$tasks, $searchQuery, $sortState, $filterStatus]) => {
+    return $tasks
+      .filter((task) => {
+        return (!$searchQuery.trim() || task.title.toLowerCase().includes($searchQuery.toLowerCase())) && ($filterStatus === "all" || ($filterStatus === "completed" && task.completed) || ($filterStatus === "notCompleted" && !task.completed));
+      })
+      .sort((a, b) => {
+        if ($sortState.key === "priority" || $sortState.key === "deadline") {
+          return $sortState.direction === "ascending" ? new Date(a[$sortState.key]) - new Date(b[$sortState.key]) : new Date(b[$sortState.key]) - new Date(a[$sortState.key]);
+        } else {
+          return $sortState.direction === "ascending" ? a[$sortState.key].localeCompare(b[$sortState.key]) : b[$sortState.key].localeCompare(a[$sortState.key]);
+        }
+      });
   });
 
-  // Click handlers to update sort state
   function updateSortState(key) {
     sortState.update((current) => ({
       key,
@@ -38,69 +36,81 @@
     }));
   }
 
-  // Editing mode
   const saveTask = (id) => {
-    let newDateString = tasks.changeDateFormat(editedDeadline);
-    tasks.updateTask({ id, title: editedTitle, priority: Number(editedPriority), deadline: newDateString });
+    tasks.updateTask({ id, title: editedTitle, priority: Number(editedPriority), deadline: editedDeadline });
     editingId.set(null); // Exit editing mode
   };
 
-  // Deleting a task
   const deleteTask = (id) => {
     tasks.removeTask(id);
   };
 
-  // Setting the status of task completion
   const toggleCompleted = (id) => {
     tasks.toggleTaskCompleted(id);
   };
 </script>
 
-{#if $sortedTasks.length > 0}
-  <section class="task-list">
-    <table id="todo-table">
-      <thead>
-        <tr>
-          <th class="td-task" id="sort-task" on:click={() => updateSortState("title")}>Task</th>
-          <th class="td-mini-sr" id="sort-priority" on:click={() => updateSortState("priority")}>Priority</th>
-          <th class="td-mini-sr" id="sort-deadline" on:click={() => updateSortState("deadline")}>Deadline</th>
-          <th class="td-mini">Actions</th>
+<section class="task-list">
+  <table id="todo-table">
+    <thead>
+      <tr>
+        <td colspan="4"><input id="searchInput" type="text" placeholder="Search tasks by title" bind:value={$searchQuery} /></td>
+      </tr>
+      <tr>
+        <th class="td-task" on:click={() => updateSortState("title")}>Task</th>
+        <th class="td-mini-sr" on:click={() => updateSortState("priority")}>Priority</th>
+        <th class="td-mini-sr" on:click={() => updateSortState("deadline")}>Deadline</th>
+        <th class="td-mini">Actions</th>
+      </tr>
+    </thead>
+    <tbody id="todo-body">
+      {#each $filteredAndSortedTasks as task}
+        <tr class:completed={task.completed}>
+          {#if $editingId === task.id}
+            <td><input type="checkbox" id="toggle-all" class="toggle-all" /><input type="text" bind:value={editedTitle} /></td>
+            <td><input type="number" bind:value={editedPriority} min="1" max="5" /></td>
+            <td><input type="date" bind:value={editedDeadline} /></td>
+            <td>
+              <button on:click={() => saveTask(task.id)}>Save</button>
+            </td>
+          {:else}
+            <td class="td-task-main"><label class="form-control"> <input type="checkbox" bind:checked={task.completed} name="checkbox-checked" on:click={() => toggleCompleted(task.id)} />{task.title}</label></td>
+            <td class="priority-{task.priority}">{task.priority}</td>
+            <td>{task.deadline}</td>
+            <td class="td-mini">
+              <button
+                on:click={() => {
+                  editingId.set(task.id);
+                  editedTitle = task.title;
+                  editedPriority = task.priority;
+                  editedDeadline = task.deadline;
+                }}>Edit</button
+              >
+              <button on:click={() => deleteTask(task.id)}>Delete</button>
+            </td>
+          {/if}
         </tr>
-      </thead>
-      <tbody id="todo-body">
-        {#each $sortedTasks as task}
-          <tr class:completed={task.completed}>
-            {#if $editingId === task.id}
-              <td><input type="text" bind:value={editedTitle} /></td>
-              <td><input type="number" bind:value={editedPriority} min="1" max="5" /></td>
-              <td><input type="date" bind:value={editedDeadline} /></td>
-              <td>
-                <button on:click={() => saveTask(task.id)}>Save</button>
-              </td>
-            {:else}
-              <td class="td-task" on:click={() => toggleCompleted(task.id)}>{task.title}</td>
-              <td class="priority-{task.priority}">{task.priority}</td>
-              <td>{task.deadline}</td>
-              <td class="td-mini">
-                <button
-                  on:click={() => {
-                    editingId.set(task.id);
-                    editedTitle = task.title;
-                    editedPriority = task.priority;
-                    editedDeadline = task.deadline;
-                  }}>Edit</button
-                >
-                <button on:click={() => deleteTask(task.id)}>Delete</button>
-              </td>
-            {/if}
-          </tr>
-        {/each}
-      </tbody>
-    </table>
-  </section>
-{:else}
-  <p>No todos yet. Add some!</p>
-{/if}
+      {/each}
+      {#if $filteredAndSortedTasks.length === 0}
+        <tr>
+          <td colspan="4">No matching tasks found.</td>
+        </tr>
+      {/if}
+      <tr>
+        <td colspan="4">
+          <nav>
+            <a href="#" on:click|preventDefault={() => filterStatus.set("all")}>All</a> |
+            <a href="#" on:click|preventDefault={() => filterStatus.set("completed")}>Completed</a> |
+            <a href="#" on:click|preventDefault={() => filterStatus.set("notCompleted")}>Not Completed</a>
+          </nav>
+        </td>
+      </tr>
+      <tr>
+        <td colspan="4"> Total: {$filteredAndSortedTasks.length} tasks </td>
+      </tr>
+    </tbody>
+  </table>
+</section>
 
 <style>
   .completed {
