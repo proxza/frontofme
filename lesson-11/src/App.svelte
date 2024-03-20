e65451b5d3ddb01e1e778608721086ab2916cdb0
Michael B. <proxza@gmail.com>
2024-03-20 19:14:59 +1100

    Add new Secret Project for tests

--- lesson-11/src/App.svelte
+++ lesson-11/src/App.svelte
@@ -18,13 +18,6 @@
     margin: 0 auto;
   }
 
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
-  }
-
   @media (max-width: 640px) {
     main {
       max-width: none;
--- lesson-11/src/components/AddTask.svelte
+++ lesson-11/src/components/AddTask.svelte
@@ -1,26 +1,33 @@
 <script>
   import { tasks } from "./store";
 
-  let title = "";
-  let priority;
-  let deadline = "";
+  let task = {
+    title: "",
+    priority: "",
+    deadline: "",
+  };
 
   function addNewTask() {
-    let newDateString = tasks.changeDateFormat(deadline);
+    const newTask = {
+      ...task,
+      id: Date.now(),
+      priority: Number(task.priority),
+      status: false,
+    };
+    tasks.addTask(newTask);
+    resetForm();
+  }
 
-    tasks.addTask({ id: Date.now(), title, priority: Number(priority), deadline: newDateString, status: false });
-    // Form cleaning
-    title = "";
-    priority = "";
-    deadline = "";
+  function resetForm() {
+    task = { title: "", priority: "", deadline: "" };
   }
 </script>
 
 <section class="add-task">
   <form on:submit|preventDefault={addNewTask}>
-    <input type="text" id="new-task" placeholder="What needs to be done?" bind:value={title} autofocus />
-    <input type="number" id="priority" placeholder="Priority" min="1" max="5" bind:value={priority} />
-    <input type="date" id="deadline" placeholder="Deadline" bind:value={deadline} />
+    <input type="text" placeholder="What needs to be done?" bind:value={task.title} autofocus />
+    <input type="number" placeholder="Priority" min="1" max="5" bind:value={task.priority} />
+    <input type="date" placeholder="Deadline" bind:value={task.deadline} />
     <button type="submit">ADD</button>
   </form>
 </section>
