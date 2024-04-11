57176936ba97ffa4f20bcd72c288277c8e6425a8
Michael B. <proxza@gmail.com>
2024-04-11 19:36:21 +1000

    Slider

---
+++ pet/myuniquecars/src/routes/acp.svelte
@@ -0,0 +1,13 @@
+<script>
+    // JavaScript goes here
+    let name = "world";
+  </script>
+  
+  <style>
+    /* CSS goes here */
+    h1 {
+      color: purple;
+    }
+  </style>
+  
+  <h1>Hello {name}!</h1>
