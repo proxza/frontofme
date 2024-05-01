4e67a0803b8b857453470c133ab0adf5c413a9ef
Michael B. <proxza@gmail.com>
2024-05-01 19:20:22 +1000

    Add next-auth

--- pet/proxzacom/components/Post.tsx
+++ pet/proxzacom/components/Post.tsx
@@ -33,7 +33,7 @@ export default function Post({ id, author, date, thumbnail, authorEmail, title, 
       <p className="content">{content}</p>
 
       {category && (
-        <Link className="bg-cyan-700 w-fit text-white px-4 py-0.5 text-sm font-bold rounded-md mt-4 block" href={`hashtegs/${category}`}>
+        <Link className="bg-cyan-600 w-fit text-white px-4 py-0.5 text-sm font-bold rounded-md mt-4 block" href={`hashtegs/${category}`}>
           #{category}
         </Link>
       )}
