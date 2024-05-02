a2ce4eb8c53289f629ad71a32d10f49901e10e93
Michael B. <proxza@gmail.com>
2024-05-02 23:16:49 +1000

    Add popup account window

--- pet/proxzacom/components/Navbar.tsx
+++ pet/proxzacom/components/Navbar.tsx
@@ -2,12 +2,35 @@
 
 import Link from "next/link";
 import { useSession, signOut } from "next-auth/react";
+import Image from "next/image";
+import { useRef, useState, useEffect } from "react";
 
 export default function Navbar() {
-  const { status } = useSession();
+  const { status, data: session } = useSession();
+  const [isPopupVisible, setIsPopupVisible] = useState(false); // Show OR hide our popup account window
+  const popupRef = useRef<HTMLDivElement | null>(null);
+
+  // Disable our popup window when we are click
+  useEffect(() => {
+    const handleClickOutside = (e: MouseEvent) => {
+      if (popupRef.current && !popupRef.current.contains(e.target as Node)) {
+        setIsPopupVisible(false);
+      }
+    };
+
+    document.addEventListener("click", handleClickOutside);
+
+    if (!isPopupVisible) {
+      document.removeEventListener("click", handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, [isPopupVisible]);
 
   return (
-    <div className="flex justify-between pb-4 border-b mb-4">
+    <div className="flex justify-between pb-4 border-b mb-4 relative">
       <div>
         <Link href={"/"}>
           <h1 className="text-dark text-4xl font-bold tracking-tighter">ProxzaVault</h1>
@@ -19,11 +42,32 @@ export default function Navbar() {
       </div>
 
       {status === "authenticated" ? (
-        <div>
-          <button onClick={() => signOut()} className="btn">
-            Sign Out
-          </button>
-        </div>
+        <>
+          <div ref={popupRef} className={`absolute z-30 right-0 top-20 bg-white p-6 shadow-lg rounded-md flex-col gap-2 text-right min-w-[160px] ${isPopupVisible ? "flex" : "hidden"}`}>
+            <div className="font-bold">{session?.user?.name}</div>
+            <Link onClick={() => setIsPopupVisible(false)} className="hover:underline" href={"/dashboard"}>
+              Dashboard
+            </Link>
+            <Link onClick={() => setIsPopupVisible(false)} className="hover:underline" href={"/profile"}>
+              Profile
+            </Link>
+            <button onClick={() => signOut()} className="btn">
+              Sign Out
+            </button>
+          </div>
+
+          <div className="flex gap-2 items-center">
+            <Link className="hidden md:flex gap-2 items-center mr-6" href={"/create"}>
+              <span>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                </svg>
+              </span>
+              <span className="font-bold">Create</span>
+            </Link>
+            <Image onClick={() => setIsPopupVisible((prev) => !prev)} src={session?.user?.image || ""} width={36} height={36} alt="Profile Image" className="rounded-full cursor-pointer" />
+          </div>
+        </>
       ) : (
         <div className="flex items-center">
           <Link className="btn" href={"/sign-in"}>
