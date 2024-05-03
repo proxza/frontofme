beaa082a6c93bf543c53fd6f158009cb44e5b262
Michael B. <proxza@gmail.com>
2024-05-03 19:05:58 +1000

    Add prisma

--- pet/proxzacom/app/api/auth/[...nextauth]/route.ts
+++ pet/proxzacom/app/api/auth/[...nextauth]/route.ts
@@ -1,16 +1,21 @@
 import NextAuth, { AuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import prisma from "@/lib/prismadb";
 
 export const authOptions: AuthOptions = {
+  adapter: PrismaAdapter(prisma),
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_CLIENT_ID as string,
       clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+      allowDangerousEmailAccountLinking: true,
     }),
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      allowDangerousEmailAccountLinking: true,
     }),
   ],
   pages: {
