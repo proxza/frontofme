c632eab6a1c3e3057a4ddec3761f9c0ede39332e
proxza <proxza@gmail.com>
2024-05-10 19:50:15 +1000

    Add cloudinary api

--- pet/proxzacom/components/CreatePostForm.tsx
+++ pet/proxzacom/components/CreatePostForm.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { TCategory } from "@/app/types";
+import { CldUploadButton, CloudinaryUploadWidgetResults } from "next-cloudinary";
+import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
@@ -24,6 +26,36 @@ export default function CreatePostForm() {
 
     fetchAllCategories();
   }, []);
+
+  const handleImageUpload = (result: CloudinaryUploadWidgetResults) => {
+    const info = result.info as object;
+
+    if ("secure_url" in info && "public_id" in info) {
+      const url = info.secure_url as string;
+      const public_id = info.public_id as string;
+      setImageUrl(url);
+      setPublicId(public_id);
+    }
+  };
+
+  const removeImage = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    try {
+      const res = await fetch("api/removeImage", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ publicId }),
+      });
+
+      if (res.ok) {
+        setImageUrl("");
+        setPublicId("");
+      }
+    } catch (error) {
+      console.log("[ERROR] ", error);
+    }
+  };
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
@@ -63,13 +95,29 @@ export default function CreatePostForm() {
     <div>
       <h2>Create Post</h2>
       <form onSubmit={handleSubmit} className="flex flex-col gap-2">
-        <input onChange={e => setTitle(e.target.value)} type="text" placeholder="Title" />
-        <textarea onChange={e => setContent(e.target.value)} placeholder="Content"></textarea>
+        <input onChange={(e) => setTitle(e.target.value)} type="text" placeholder="Title" />
+        <textarea onChange={(e) => setContent(e.target.value)} placeholder="Content"></textarea>
 
-        <select onChange={e => setSelectedCategory(e.target.value)} className="p-3 rounded-md border appearance-none">
+        <CldUploadButton uploadPreset={process.env.CLOUDINARY_UPLOAD_PRESET} className={`h-48 border-2 mt-4 border-dotted grid place-items-center bg-slate-100 rounded-md relative ${imageUrl && "pointer-events-none"}`} onUpload={handleImageUpload}>
+          <div>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="m2.25 15.75 5.159-5.159a2.25 2.25 0 0 1 3.182 0l5.159 5.159m-1.5-1.5 1.409-1.409a2.25 2.25 0 0 1 3.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 0 0 1.5-1.5V6a1.5 1.5 0 0 0-1.5-1.5H3.75A1.5 1.5 0 0 0 2.25 6v12a1.5 1.5 0 0 0 1.5 1.5Zm10.5-11.25h.008v.008h-.008V8.25Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
+            </svg>
+          </div>
+
+          {imageUrl && <Image src={imageUrl} fill alt="img" className="absolute object-cover inset-0" />}
+        </CldUploadButton>
+
+        {publicId && (
+          <button onClick={removeImage} className="py-2 px-4 rounded-md font-bold w-fit bg-red-600 text-white mb-4">
+            Remove Image
+          </button>
+        )}
+
+        <select onChange={(e) => setSelectedCategory(e.target.value)} className="p-3 rounded-md border appearance-none">
           <option value="">Select a hashtag</option>
           {categories &&
-            categories.map(category => (
+            categories.map((category) => (
               <option key={category.id} value={category.catName}>
                 {category.catName}
               </option>
