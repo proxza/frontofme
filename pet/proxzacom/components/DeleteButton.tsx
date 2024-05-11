78240d0d9fc2632134728056747fea49adae8d37
proxza <proxza@gmail.com>
2024-05-11 19:42:16 +1000

    Updates

--- pet/proxzacom/components/DeleteButton.tsx
+++ pet/proxzacom/components/DeleteButton.tsx
@@ -4,6 +4,14 @@ import { useRouter } from "next/navigation";
 
 export default function DeleteButton({ id }: { id: string }) {
   const router = useRouter();
+
+  const deleteImage = async (publicId: string) => {
+    const res = await fetch("/api/removeImage", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ publicId }),
+    });
+  };
   const handleDelete = async () => {
     const confirmed = window.confirm("Are you sure you want to delete this post?");
 
@@ -18,6 +26,10 @@ export default function DeleteButton({ id }: { id: string }) {
 
         if (res.ok) {
           console.log("[DEBUG] Post deleted.");
+          const post = await res.json();
+          const { publicId } = post;
+          await deleteImage(publicId);
+
           router.refresh(); // Reload our page
         }
       } catch (error) {
